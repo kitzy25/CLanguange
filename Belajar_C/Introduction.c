c38fae18db1098052ef757115c8e021963f85ac2
Felisca Neldy <118001528+kitzy25@users.noreply.github.com>
2025-06-05 18:30:44 +0700

    Add files via upload

---
+++ Belajar_C/Introduction.c
@@ -0,0 +1,20 @@
+#include<stdio.h> // HEADER untuk input/output
+
+int main(){ // Main Function
+
+    // HELLO WORLD!
+    // Printf -> Sistem akan ngeprint sesuai dengan data yang kita input
+    printf("Hello World!\n"); 
+    printf("\tWelcome to the C\n");
+    printf("Welcome to Coding \"JAVA\".");
+
+    // COMMAND
+    // // -> untuk single line comment
+    // /* */ -> untuk multi line comment
+
+    
+    // \n -> untuk enter atau bikin line baru
+    // \t -> untuk mengetab atau seperti membuat paragraf
+    return 0; // untuk ngereturn
+}
+
